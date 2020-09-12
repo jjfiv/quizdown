63590ec8664f387257587c00a03e359abfdf6459
John Foley <johnf@middlebury.edu>
2020-09-12 09:10:47 -0400

    preliminary qti support

--- cffi/package.sh
+++ cffi/package.sh
@@ -4,6 +4,7 @@ set -eu
 
 rm -rf quizdown/quizdown
 source venv/bin/activate
+pip install -q -r requirements.txt
 pip install -q -r dev-requirements.txt
 maturin build -b cffi --release
 maturin develop -b cffi --release
